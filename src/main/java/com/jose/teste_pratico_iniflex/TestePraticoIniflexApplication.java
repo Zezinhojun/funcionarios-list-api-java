0f915b71c7ac1882cc24d5a53a4df2d12611fead
Junior Silva <zezinhojun@gmail.com>
2024-06-24 13:28:51 -0300

    Swagger configurado e documentado com sucesso

--- src/main/java/com/jose/teste_pratico_iniflex/TestePraticoIniflexApplication.java
+++ src/main/java/com/jose/teste_pratico_iniflex/TestePraticoIniflexApplication.java
@@ -3,7 +3,11 @@ package com.jose.teste_pratico_iniflex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Swagger OpenApi", version = "1", description = "API desenvolvida para o teste pratico iniflex"))
 public class TestePraticoIniflexApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/jose/teste_pratico_iniflex/controller/FuncionarioController.java
+++ src/main/java/com/jose/teste_pratico_iniflex/controller/FuncionarioController.java
@@ -18,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jose.teste_pratico_iniflex.dto.FuncionarioDTO;
 import com.jose.teste_pratico_iniflex.service.FuncionarioService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 
@@ -25,12 +28,17 @@ import lombok.AllArgsConstructor;
 @RestController
 @Configuration
 @RequestMapping("/api/funcionarios")
+@Tag(name = "Funcionários", description = "APIs para gerenciamento de funcionários")
 @AllArgsConstructor
 
 public class FuncionarioController {
 
     private final FuncionarioService funcionarioService;
 
+    @Operation(summary = "Insere funcionarios fictícios", description = "Insere funcionarios fictícios na base de dados.", responses = {
+            @ApiResponse(responseCode = "200", description = "Lista de funcionários retornada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao processar a solicitação"),
+    })
     @GetMapping("/inserir-funcionarios")
     public ResponseEntity<String> inserirFuncionarios() {
         List<FuncionarioDTO> funcionariosInseridos = funcionarioService.gerarFuncionariosFicticios();
@@ -39,11 +47,23 @@ public class FuncionarioController {
         return ResponseEntity.ok(mensagem);
     }
 
+    @Operation(summary = "Remove um funcionário pelo nome", description = "Deleta um funcionário da base de dados com base no nome fornecido.", responses = {
+            @ApiResponse(responseCode = "204", description = "Funcionário removido com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro ao processar a solicitação"),
+            @ApiResponse(responseCode = "404", description = "Funcionário não encontrado.")
+    })
     @DeleteMapping("/{nome}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removerFuncionarioPorNome(@PathVariable @NotNull String nome) {
         funcionarioService.removerFuncionarioPorNome(nome);
     }
+
+    @Operation(summary = "Listar todos os funcionários", description = " Retorna uma lista de todos os funcionários cadastrados.", responses = {
+            @ApiResponse(responseCode = "200", description = "Lista de funcionários retornada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao processar a solicitação"),
+            @ApiResponse(responseCode = "404", description = "Não foram encontrados funcionários."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
 
     @GetMapping("")
     public ResponseEntity<List<FuncionarioDTO>> listarTodosOsFuncionarios() {
@@ -51,23 +71,48 @@ public class FuncionarioController {
         return ResponseEntity.ok(funcionariosDTO);
     }
 
+    @Operation(summary = "Aumentar salário de todos os funcionários em 10%", description = "Aumenta o salário de todos os funcionários em 10%.", responses = {
+            @ApiResponse(responseCode = "200", description = "Salários aumentados com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao processar a solicitação"),
+            @ApiResponse(responseCode = "404", description = "Não foram encontrados funcionários."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping("/aumentar-salario")
     public ResponseEntity<List<FuncionarioDTO>> aumentarSalario() {
         List<FuncionarioDTO> funcionariosAtualizados = funcionarioService.aumentarSalario();
         return ResponseEntity.ok(funcionariosAtualizados);
     }
 
+    @Operation(summary = "Agrupar funcionários por função", description = "Agrupa os funcionários por função.", responses = {
+            @ApiResponse(responseCode = "200", description = "Funcionários agrupados com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao processar a solicitação"),
+            @ApiResponse(responseCode = "404", description = "Não foram encontrados funcionários."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping("/agrupados")
     public ResponseEntity<Map<String, List<FuncionarioDTO>>> getFuncionariosAgrupados() {
         Map<String, List<FuncionarioDTO>> funcionariosAgrupados = funcionarioService.agruparPorFuncao();
         return ResponseEntity.ok(funcionariosAgrupados);
     }
 
+    @Operation(summary = "Listar funcionários aniversariantes", description = "Retorna uma lista de funcionários que fazem aniversário no mês atual.", responses = {
+            @ApiResponse(responseCode = "200", description = "Lista de aniversariantes retornada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao processar a solicitação"),
+            @ApiResponse(responseCode = "404", description = "Não foram encontrados funcionários."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping("/aniversariantes")
     public ResponseEntity<List<FuncionarioDTO>> listarFuncionariosAniversariantes() {
         List<FuncionarioDTO> aniversariantes = funcionarioService.listarFuncionariosAniversariantes();
         return ResponseEntity.ok(aniversariantes);
     }
+
+    @Operation(summary = "Obter funcionário de maior idade", description = "Retorna o funcionário de maior idade.", responses = {
+            @ApiResponse(responseCode = "200", description = "Funcionario com maior idade retornado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao processar a solicitação"),
+            @ApiResponse(responseCode = "404", description = "Não foram encontrados funcionários."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
 
     @GetMapping("/maior-idade")
     public ResponseEntity<Map<String, Object>> funcionarioMaiorIdade() {
@@ -75,18 +120,36 @@ public class FuncionarioController {
         return ResponseEntity.ok(funcionarioDetalhes);
     }
 
+    @Operation(summary = "Listar funcionários em ordem alfabética", description = "Retorna uma lista de funcionários ordenada alfabeticamente.", responses = {
+            @ApiResponse(responseCode = "200", description = "Lista de funcionários retornada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao processar a solicitação"),
+            @ApiResponse(responseCode = "404", description = "Não foram encontrados funcionários."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping("/ordenado-alfabetica")
     public ResponseEntity<List<FuncionarioDTO>> listarFuncionariosOrdenAlfabetica() {
         List<FuncionarioDTO> funcionariosOrdenados = funcionarioService.listarFuncionariosOrdenAlfabetica();
         return ResponseEntity.ok(funcionariosOrdenados);
     }
 
+    @Operation(summary = "Obter o total dos salários dos funcionários", description = "Retorna o valor total dos salários de todos os funcionários.", responses = {
+            @ApiResponse(responseCode = "200", description = "Total de salários retornado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao processar a solicitação"),
+            @ApiResponse(responseCode = "404", description = "Não foram encontrados funcionários."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping("/total-salarios")
     public ResponseEntity<BigDecimal> getTotalSalarios() {
         BigDecimal totalSalarios = funcionarioService.calcularTotalSalarios();
         return ResponseEntity.ok(totalSalarios);
     }
 
+    @Operation(summary = "Calcular salários mínimos dos funcionários", description = "Calcula quantos salários mínimos cada funcionário recebe.", responses = {
+            @ApiResponse(responseCode = "200", description = "Salários mínimos calculados com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao processar a solicitação"),
+            @ApiResponse(responseCode = "404", description = "Não foram encontrados funcionários."),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping("/salarios-minimos")
     public ResponseEntity<List<Map<String, Object>>> calcularSalariosMinimosDosFuncionarios() {
         List<Map<String, Object>> salariosMinimos = funcionarioService
